65df63043463bce972f9c57aba6c9180c355f2ee
Juan <jroussilian@fi.uba.ar>
2023-05-02 03:40:09 -0300

    Refactor de creacion de contenedores en main

--- src/main.rs
+++ src/main.rs
@@ -19,52 +19,79 @@ fn main() {
 
     if let Ok(mut lista_pedidos) = PedidosParser::new(&data).obtener_pedidos() {
         pedidos = Arc::new(RwLock::new(lista_pedidos));
-    }
-    let contenedor_cafe;
-    if let Ok(contenedor_granos_cafe) =
-        ContenedorRecarga::new(CAPACIDAD_CAFE_GRANO, CAPACIDAD_CAFE_GRANO)
-    {
-        match Contenedor::new(
-            CAPACIDAD_CAFE_MOLIDO,
-            CAPACIDAD_CAFE_MOLIDO,
-            contenedor_granos_cafe,
-        ) {
-            Ok(contenedor_cafe_molido) => {
-                contenedor_cafe = Arc::new(RwLock::new(contenedor_cafe_molido))
-            }
+    };
+
+    let contenedor_cafe =
+        match crear_arc_lock_contenedor(CAPACIDAD_CAFE_MOLIDO, CAPACIDAD_CAFE_GRANO) {
+            Ok(contenedor_arc) => contenedor_arc,
             Err(e) => {
-                println!("{}: Revisar valores archivo de constantes", e);
+                println!(e);
                 return;
             }
+        };
+    let contenedor_espuma =
+        match crear_arc_lock_contenedor(CAPACIDAD_LECHE_ESPUMA, CAPACIDAD_LECHE_FRIA) {
+            Ok(contenedor_arc) => contenedor_arc,
+            Err(e) => {
+                println!(e);
+                return;
+            }
+        };
+    let contenedor_agua =
+        match crear_arc_lock_contenedor(CAPACIDAD_AGUA_CALIENTE, f64::INFINITY as u32) {
+            Ok(contenedor_arc) => contenedor_arc,
+            Err(e) => {
+                println!(e);
+                return;
+            }
+        };
+    let contenedor_cacao = match crear_arc_lock_contenedor(CAPACIDAD_CACAO, 0) {
+        Ok(contenedor_arc) => contenedor_arc,
+        Err(e) => {
+            println!(e);
+            return;
         }
-    }
-    let contenedor_espuma;
-    if let Ok(contenedor_leche) = ContenedorRecarga::new(CAPACIDAD_LECHE_FRIA, CAPACIDAD_LECHE_FRIA)
-    {
-        match Contenedor::new(
-            CAPACIDAD_LECHE_ESPUMA,
-            CAPACIDAD_LECHE_ESPUMA,
-            contenedor_leche,
-        ) {
-            Ok(contenedor_espuma_leche) => {
-                contenedor_espuma = Arc::new(RwLock::new(contenedor_espuma_leche))
+    };
+}
+
+pub fn crear_arc_lock_contenedor(
+    capacidad_contenedor: u32,
+    capacidad_contenedor_rec: u32,
+) -> Result<Arc<RwLock<Contenedor>>, String> {
+    let contenedor_final;
+    if capacidad_contenedor_rec > 0 {
+        match ContenedorRecarga::new(capacidad_contenedor_rec, capacidad_contenedor_rec) {
+            Ok(contenedor_recarga) => {
+                match Contenedor::new(
+                    capacidad_contenedor,
+                    capacidad_contenedor,
+                    Some(contenedor_recarga),
+                ) {
+                    Ok(contenedor) => {
+                        contenedor_final = Arc::new(RwLock::new(contenedor));
+                        Ok(contenedor_final)
+                    }
+                    Err(e) => Err(format!(
+                        "[Error]{}: Revisar valores de constantes en constants.rs",
+                        e
+                    )),
+                }
             }
-            Err(e) => {
-                println!("{}: Revisar valores archivo de constantes", e);
-                return;
-            }
+            Err(e) => Err(format!(
+                "[Error]{}: Revisar valores de constantes en constants.rs",
+                e
+            )),
         }
-    }
-    let contenedor_agua;
-    if let Ok(red_agua) = ContenedorRecarga::new(f64::INFINITY as u32, f64::INFINITY as u32) {
-        match Contenedor::new(CAPACIDAD_AGUA_CALIENTE, CAPACIDAD_AGUA_CALIENTE, red_agua) {
-            Ok(contenedor_agua_caliente) => {
-                contenedor_agua = Arc::new(RwLock::new(contenedor_agua_caliente))
+    } else {
+        match Contenedor::new(capacidad_contenedor, capacidad_contenedor, None) {
+            Ok(contenedor) => {
+                contenedor_final = Arc::new(RwLock::new(contenedor));
+                Ok(contenedor_final)
             }
-            Err(e) => {
-                println!("{}: Revisar valores archivo de constantes", e);
-                return;
-            }
+            Err(e) => Err(format!(
+                "[Error]{}: Revisar valores de constantes en constants.rs",
+                e
+            )),
         }
     }
 }
