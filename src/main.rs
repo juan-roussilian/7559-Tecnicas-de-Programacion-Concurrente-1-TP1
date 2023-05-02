a931b3cf4dea1b9a230c743b28d8b2392a1e071f
Juan <jroussilian@fi.uba.ar>
2023-05-02 04:02:51 -0300

    Agrego la creacion de N threads

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,21 @@ use crate::constants::*;
 use crate::contenedor::Contenedor;
 use crate::contenedor_recarga::ContenedorRecarga;
 use crate::pedidos_parser::PedidosParser;
-use std::fs;
 use std::sync::{Arc, RwLock};
+use std::{fs, thread};
 
 fn main() {
     let path = "pedidos_ejemplo.json";
     let data = fs::read_to_string(path).expect("Unable to read file");
+    let _pedidos_preparados = Arc::new(RwLock::new(0));
+    let _grano_cafe_consumido = Arc::new(RwLock::new(0));
+    let _leche_fria_consumida = Arc::new(RwLock::new(0));
+    let _agua_de_red_consumida = Arc::new(RwLock::new(0));
+    let _cafe_molido_consumido = Arc::new(RwLock::new(0));
+    let _espuma_leche_consumido = Arc::new(RwLock::new(0));
+    let _agua_caliente_consumida = Arc::new(RwLock::new(0));
+    let _cacao_consumido = Arc::new(RwLock::new(0));
+
     let pedidos;
 
     if let Ok(mut lista_pedidos) = PedidosParser::new(&data).obtener_pedidos() {
@@ -25,7 +34,7 @@ fn main() {
         match crear_arc_lock_contenedor(CAPACIDAD_CAFE_MOLIDO, CAPACIDAD_CAFE_GRANO) {
             Ok(contenedor_arc) => contenedor_arc,
             Err(e) => {
-                println!(e);
+                println!("{}",e);
                 return;
             }
         };
@@ -33,7 +42,7 @@ fn main() {
         match crear_arc_lock_contenedor(CAPACIDAD_LECHE_ESPUMA, CAPACIDAD_LECHE_FRIA) {
             Ok(contenedor_arc) => contenedor_arc,
             Err(e) => {
-                println!(e);
+                println!("{}",e);
                 return;
             }
         };
@@ -41,17 +50,28 @@ fn main() {
         match crear_arc_lock_contenedor(CAPACIDAD_AGUA_CALIENTE, f64::INFINITY as u32) {
             Ok(contenedor_arc) => contenedor_arc,
             Err(e) => {
-                println!(e);
+                println!("{}",e);
                 return;
             }
         };
     let contenedor_cacao = match crear_arc_lock_contenedor(CAPACIDAD_CACAO, 0) {
         Ok(contenedor_arc) => contenedor_arc,
         Err(e) => {
-            println!(e);
+            println!("{}",e);
             return;
         }
     };
+
+    let mut dispensers = vec![];
+
+    for _ in 0..CANTIDAD_DISPENSADORES {
+        let lock_contenedor_cafe = contenedor_cafe.clone();
+        let lock_contenedor_agua = contenedor_agua.clone();
+        let lock_contenedor_espuma = contenedor_espuma.clone();
+        let lock_contenedor_cacao = contenedor_cacao.clone();
+
+        dispensers.push(thread::spawn(move || println!("A implementar"))/*dispensador_preparar(contenedores,metricas)*/)
+    }
 }
 
 pub fn crear_arc_lock_contenedor(
